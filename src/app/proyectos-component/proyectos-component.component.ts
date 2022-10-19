b17147ef48da1cbc1ed72e7356e3062306592526
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-10-19 11:11:47 -0300

    Redireccionamiento desde un boton con navigate

--- src/app/proyectos-component/proyectos-component.component.ts
+++ src/app/proyectos-component/proyectos-component.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado/empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-proyectos-component',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router,
+    private servicioEmpleado:ServicioEmpleadosService,
+    private servicioDataEmpleado: EmpleadosService) { 
+      this.empleados = this.servicioDataEmpleado.empleados;
+    }
 
   ngOnInit(): void {
   }
 
+  empleados:Empleado[] = [];
+
+  volverHome(){
+      this.router.navigate(['']);
+  }
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido,this.cuadroCargo,this.cuadroSalario)
+
+    //this.servicioEmpleado.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre);
+
+    this.servicioDataEmpleado.agregarEmpleadoServicio(miEmpleado);
+
+    this.router.navigate(['']);
+
+  }
+  cuadroNombre: string = "";
+  cuadroApellido: string = ""
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
 }
